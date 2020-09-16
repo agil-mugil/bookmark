193c0362a92adf5a73c632563c5314be39323ad7
pmurugesan15 <pmurugesan2012@gmail.com>
2020-09-16 14:31:47 +0530

    fix: remove the hard coded value in the query

--- src/main/java/com/eng/stream/hackathon/bookmark/repositories/GroupReferenceRepository.java
+++ src/main/java/com/eng/stream/hackathon/bookmark/repositories/GroupReferenceRepository.java
@@ -11,7 +11,7 @@ import com.eng.stream.hackathon.bookmark.models.GroupReferenceId;
 public interface GroupReferenceRepository extends JpaRepository<GroupReference, GroupReferenceId> {
 		@Query(value = "select distinct groupRef.groupType from GroupReference groupRef order by groupRef.groupType")
 		public String[] groupTypes();
-		@Query(value = "select * from tgrouprefdataeng gdata where gdata.GROUPTYPE='FT' AND gdata.groupvalue not in (select distinct g.GROUPVALUE from tbookmarkgroupseng g)", nativeQuery = true)
+		@Query(value = "select * from tgrouprefdataeng gdata where gdata.GROUPTYPE=?1 AND gdata.groupvalue not in (select distinct g.GROUPVALUE from tbookmarkgroupseng g)", nativeQuery = true)
 		public List<GroupReference> findByGroupType(String groupType);
 		
 }
