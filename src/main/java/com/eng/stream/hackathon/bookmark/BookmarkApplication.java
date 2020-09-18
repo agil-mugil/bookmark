b535186323e2f3c6783dff89a4dd0127a7126878
pmurugesan15 <pmurugesan2012@gmail.com>
2020-09-18 06:48:27 +0530

    data: added few reference data

--- src/main/java/com/eng/stream/hackathon/bookmark/BookmarkApplication.java
+++ src/main/java/com/eng/stream/hackathon/bookmark/BookmarkApplication.java
@@ -30,14 +30,29 @@ public class BookmarkApplication {
 	@Bean
 	InitializingBean sendDatabase() {
 	    return () -> {
-			  groupReferenceRepo.save(new GroupReference("TRIBE","RCM",  "Referntial Client Management","bookmarkApp",new Date(System.currentTimeMillis()))); 
+	    	 //Tribe details
+	    	  groupReferenceRepo.save(new GroupReference("TRIBE","RCM",  "Triber Rcm","bookmarkApp",new Date(System.currentTimeMillis()))); 
+	    	  groupReferenceRepo.save(new GroupReference("TRIBE","FRM",  "Tribe Frm","bookmarkApp",new Date(System.currentTimeMillis()))); 
+	    	  groupReferenceRepo.save(new GroupReference("TRIBE","EQD",  "Tribe Eqd","bookmarkApp",new Date(System.currentTimeMillis()))); 
+	    	  
+	    	  //Featured Team
 			  groupReferenceRepo.save(new  GroupReference("FT","SNO", "Strcture and Oraganization","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			  groupReferenceRepo.save(new GroupReference("FT","ENO",  "Environmental and Others","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			  groupReferenceRepo.save(new GroupReference("FT","PNA", "Referntial Client Management","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			  groupReferenceRepo.save(new GroupReference("FT","KYC", "Know Your Customer","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new GroupReference("FT","CMT", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new GroupReference("FT","CTA", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			  groupReferenceRepo.save(new GroupReference("FT","SCOW", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new GroupReference("FT","ONS", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new GroupReference("FT","ATS", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new GroupReference("FT","NOS", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  
+			  // Applications
 			  groupReferenceRepo.save(new GroupReference("APPLICATION","BDR",  "Referential System","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			  groupReferenceRepo.save(new  GroupReference("APPLICATION","MAESTRO", "Maestro Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new  GroupReference("APPLICATION","TPS", "Maestro Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new  GroupReference("APPLICATION","EDGE", "Maestro Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new  GroupReference("APPLICATION","RISQ", "Maestro Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			 
 	      };
 	   }
