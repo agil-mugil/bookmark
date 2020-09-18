434b8658d4b3ae2994c334921052562405941604
pmurugesan15 <pmurugesan2012@gmail.com>
2020-09-18 07:06:02 +0530

    setup: corrected the data label

--- src/main/java/com/eng/stream/hackathon/bookmark/BookmarkApplication.java
+++ src/main/java/com/eng/stream/hackathon/bookmark/BookmarkApplication.java
@@ -40,19 +40,19 @@ public class BookmarkApplication {
 			  groupReferenceRepo.save(new GroupReference("FT","ENO",  "Environmental and Others","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			  groupReferenceRepo.save(new GroupReference("FT","PNA", "Referntial Client Management","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			  groupReferenceRepo.save(new GroupReference("FT","KYC", "Know Your Customer","bookmarkApp",new Date(System.currentTimeMillis()))); 
-			  groupReferenceRepo.save(new GroupReference("FT","CMT", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
-			  groupReferenceRepo.save(new GroupReference("FT","CTA", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new GroupReference("FT","CMT", "Cmt Feature team","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new GroupReference("FT","CTA", "Cta Feature team","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			  groupReferenceRepo.save(new GroupReference("FT","SCOW", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
-			  groupReferenceRepo.save(new GroupReference("FT","ONS", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
-			  groupReferenceRepo.save(new GroupReference("FT","ATS", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
-			  groupReferenceRepo.save(new GroupReference("FT","NOS", "SGCIB Client Onboarding team","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new GroupReference("FT","ONS", "Ons FT","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new GroupReference("FT","ATS", "Ats Core","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new GroupReference("FT","NOS", "Nos Featured team","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			  
 			  // Applications
 			  groupReferenceRepo.save(new GroupReference("APPLICATION","BDR",  "Referential System","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			  groupReferenceRepo.save(new  GroupReference("APPLICATION","MAESTRO", "Maestro Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
-			  groupReferenceRepo.save(new  GroupReference("APPLICATION","TPS", "Maestro Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
-			  groupReferenceRepo.save(new  GroupReference("APPLICATION","EDGE", "Maestro Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
-			  groupReferenceRepo.save(new  GroupReference("APPLICATION","RISQ", "Maestro Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new  GroupReference("APPLICATION","TPS", "TPS Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new  GroupReference("APPLICATION","EDGE", "Edge Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
+			  groupReferenceRepo.save(new  GroupReference("APPLICATION","RISQ", "Risk Application","bookmarkApp",new Date(System.currentTimeMillis()))); 
 			 
 	      };
 	   }
